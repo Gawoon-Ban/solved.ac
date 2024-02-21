cc103265267157be7f02f4df8c48ec7f2427d2bd
Gawoon-Ban <159792104+Gawoon-Ban@users.noreply.github.com>
2024-02-21 09:53:21 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,27 @@
+def find_way(n):
+    if n <= 0:
+        return 0
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    if n == 3:
+        return 4
+
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
+
+    for i in range(4, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+
+    return dp[n]
+
+number = int(input())
+answers = []
+for j in range(number):
+    numeral = int(input())
+    answers.append(find_way(numeral))
+for k in answers:
+    print(k)

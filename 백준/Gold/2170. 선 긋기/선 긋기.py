9c7e9132b5472900cd32677a6acdf34c76322fe3
Hosii <159792104+Gawoon-Ban@users.noreply.github.com>
2024-04-22 07:52:07 +0900

    [Gold V] Title: 선 긋기, Time: 3528 ms, Memory: 259772 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.py
+++ 백준/Gold/2170. 선 긋기/선 긋기.py
@@ -7,24 +7,14 @@ for _ in range(n):
     a, b = map(int, input().split())
     pair.append((a,b))
 pair.sort(key=lambda x: (x[0], x[1]))
-ind = 0
-next = 1
-ans = 0
-while (ind <= n-1) and (next <= n-1):
-    # 안 겹칠 때:
-    if pair[ind][1] < pair[next][0]:
-        ans += pair[ind][1] - pair[ind][0]
-        ind = next
-        next += 1
-    # 겹칠 때:
+result = 0
+start, end = pair[0]
+for i in range(1, len(pair)):
+    if end < pair[i][0]:
+        result += end-start
+        start, end = pair[i]
     else:
-        # 다음 선분이 포함될 때:
-        if pair[ind][1] > pair[next][1]:
-            next += 1
+        end = max(end, pair[i][1])
+result += end-start
 
-        # 다음 선분이 더 길 때 or 끝이 같을 때:
-        else:
-            ans += pair[next][0] - pair[ind][0]
-            ind = next
-            next += 1
-print(ans+pair[ind][1]-pair[ind][0])
+print(result)
